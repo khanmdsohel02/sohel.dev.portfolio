b08bf6c2c1ebaef4e4347dcb70596000801e1771
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2024-06-09 15:55:28 +0600

    more modify

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,37 @@ import CTA from "./CTA";
 import ME from "../../assets/me.png";
 import "./header.css";
 import HeaderSocials from "./HeaderSocials";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 
 const Header = () => {
+  const el = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "MERN Stack Developer",
+        "Frontend ReactJS Developer",
+        "Backend Developer",
+      ],
+      typeSpeed: 70,
+      backSpeed: 70,
+      backDelay: 1500,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <header>
       <div className="container header__container">
         <h5>Hello Dear I&apos;m</h5>
         <h1>Md. SoHeL</h1>
-        <h5 className="text-light">MERN Stack Developer</h5>
+        <h5 className="text-light">
+          An <span ref={el} />
+        </h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
